51337b95e34f77ffdcbb28a86d084615908bce70
Erick Ramirez <erickardus@gmail.com>
2022-04-20 17:32:09 -0500

    Add workspace suffix

--- data.tf
+++
@@ -1,6 +0,0 @@
-# Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-#
-data "local_file" "version" {
-  filename = "${path.module}/VERSION"
-}
